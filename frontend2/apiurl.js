e1c1e0ca5f742910a6245b642000f99de00c4960
Majid Husain <majidsmart7@gmail.com>
2025-04-12 16:34:48 +0530

    crt

--- frontend2/apiurl.js
+++ frontend2/apiurl.js
@@ -1,5 +1,6 @@
 
-export const API_BASE_URL = "https://busmate-2-acol.onrender.com";
+export const API_BASE_URL = "https://busmate-2.onrender.com"; // Ensure this is your correct API URL
+export const API_BASE2 = "https://busmate-2-acol.onrender.com"; 
 
 
 //thamil - http://192.168.39.6:5000
