2697056035269d67cc6f576e60534457933229d4
Majid Husain <majidsmart7@gmail.com>
2025-04-12 16:10:53 +0530

    mm

--- server1/index.js
+++ server1/index.js
@@ -29,7 +29,8 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // API Routes
