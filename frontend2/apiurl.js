22f061be9c6fcd60da7f2c8aae6d4a4050b4a519
Majid Husain <majidsmart7@gmail.com>
2025-04-24 18:02:29 +0530

    check

--- frontend2/apiurl.js
+++ frontend2/apiurl.js
@@ -1,9 +1,8 @@
 
-export const API_BASE_URL = "http://192.168.8.6:5000"; // Ensure this is your correct API URL
+export const API_BASE_URL = "http://192.168.8.6:5000"; 
 export const API_BASE2 = "http://192.168.8.6:3000"; 
+
 
 // Host 1 =  "https://busmate-2.onrender.com"
 // Host 2 =  "https://busmate-2-acol.onrender.com"
-
-
-//thamil - http://192.168.39.6:5000y
+// thamil - http://192.168.39.6:5000
